791647030fc07b27c6ceb27a5d5d1194b17870f8
Collin <collinhodgs@gmail.com>
2020-06-28 01:35:34 -0600

    began ajax function to get api data

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,42 @@ let getDate = function (days) {
   
     return mm + " / " + dd + " / " + y;
   };
+
+  $(document).ready(function () {
+    var currentConditions = function (cityName, searched) {
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
+        success: function (result) {
+          if (searched === true) {
+            if (searchedCities.includes($("#search input").val()) !== true) {
+              searchedCities.push($("#search input").val());
+              localStorage.setItem("citySearch", JSON.stringify(searchedCities));
+            }
+            localStorage.setItem("lastCitySearch", $("#search input").val());
+          }
+  
+          cityId = result.id;
+          $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}&units=imperial`,
+            success: function (result) {
+              $("#today").html("");
+              $("#today").append(
+                `<div class="blockHeading"><h2>${result.city.name} ( ${getDate(
+                  0
+                )} )</h2><img src="https://openweathermap.org/img/w/${
+                  result.list[0].weather[0].icon
+                }.png" alt="${
+                  result.list[0].weather[0].description
+                }" width='50' height='50'>`
+              );
+              $("#today").append(
+                `<p class="temperature">Temperature: ${result.list[0].main.temp} °F</p>`
+              );
+              $("#today").append(
+                `<p class="humidity"> Humidity: ${result.list[0].main.humidity} %</p>`
+              );
+              $("#today").append(
+                `<p class="wind_speed">Wind Speed:  ${mph(
+                  result.list[0].wind.speed
+                )} MPH</p>`
+              );  
