a30ed64df2c3929bb7084144bab746302a02d3bc
Collin <collinhodgs@gmail.com>
2020-07-13 11:44:02 -0600

    Added eventlistener

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
 var apiKey = "86c2386f7cd01320261c251552adbfb4";
+var currentURL = "https://api.openweathermap.org/data/2.5/weather?";
+var searchBTN = document.getElementById("searchIcon");
+
+searchBTN.addEventListener('click',function(event){
+  // console.log("I was clicked")
+  var userInput = document.getElementById("searchText").value;
+  // fetch(currentURL+"q="+userInput+"&appid="+apiKey)
+  fetch(currentURL+`q=${userInput}&appid=${apiKey}`)
+  .then(function(response){
+    return response.json()
+  }).then(function(data){
+    // console.log(data.weather[0].description);
+   var todayContainer = document.getElementById("today");
+  var h1El = document.createElement('h1');
+  // 
+  h1El.value=data.name;
+  todayContainer.appendChild(h1El);
+
+  })
+});
