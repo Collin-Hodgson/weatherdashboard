6c0e05bece5942262ca0b66e094586db6d87c2a4
Collin <collinhodgs@gmail.com>
2020-07-15 11:55:35 -0600

    Appended new tags with classes and styled

--- assets/script.js
+++ assets/script.js
@@ -10,24 +10,69 @@ var cities = JSON.parse(localStorage.getItem("cities")) || [];
 var searchBTN = document.getElementById("searchIcon");
 var todayContainer = document.getElementById("today");
 function displayCurrentWeather(data) {
+  $("#today").html("");
+  console.log(data);
+  let lat = data["coord"]["lat"];
+  let lon = data["coord"]["lon"];
+  fetch(uvIndex + `&lat=${lat}&lon=${lon}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayUV(data);
+    });
   $("#today").append(
     "<h2>" +
       data["name"] +
       "</h2>" +
       "<p>Temperature: " +
       data["main"]["temp"] / 10 +
-      " F</p>" +
+      " F </p>" +
       "<p>Humidity: " +
       data["main"]["humidity"] +
-      "</p>" +
+      " % </p>" +
       "<p>Wind Speed: " +
       data["wind"]["speed"] +
-      "MPH </p>"
+      " MPH </p>"
   );
 }
 
 function displayUV(data) {
-  $("#today").append("<p>UV: " + test + "</p>");
+  console.log(data);
+
+  if (data["value"] < 5) {
+    $("#today").append(
+      "<p>UV: <span class='lowUV'>" + data["value"] + "</span></p>"
+    );
+  } else if (data["value"] < 10) {
+    $("#today").append(
+      "<p>UV: <span class='midUV'>" + data["value"] + "</span></p>"
+    );
+  } else {
+    $("#today").append(
+      "<p>UV: <span class='highUV'>" + data["value"] + "</span></p>"
+    );
+  }
+}
+
+function displayFiveDay(data) {
+  $("#fiveDays").html("");
+  console.log(data);
+  for (let i = 0; i < 5; i++) {
+    $("#fiveDays").append(
+      "<div class='days'>" +
+        "<p>Temperature: " +
+        data["list"][i]["main"]["temp"] / 10 +
+        " F </p>" +
+        "<p>Humidity: " +
+        data["list"][i]["main"]["humidity"] +
+        " % </p>" +
+        "<p>Wind Speed: " +
+        data["list"][i]["wind"]["speed"] +
+        " MPH </p>" +
+        "</div>"
+    );
+  }
 }
 searchBTN.addEventListener("click", function (_event) {
   var userInput = document.getElementById("searchText").value;
@@ -41,11 +86,12 @@ searchBTN.addEventListener("click", function (_event) {
       displayCurrentWeather(data);
       //displayFiveDayForecast(userInput);
     });
-  fetch(uvIndex)
+
+  fetch(fiveDayURL + `&q=${userInput}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      displayUV(data);
+      displayFiveDay(data);
     });
 });
